8cc92497b6f4a9bbafd024fb51e1c0f9ccd5cbdb
xsto93 <xsto93@gmail.com>
2022-12-14 08:21:59 +0100

    Update routes.js

--- api/routes.js
+++ api/routes.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const { default: mongoose } = require("mongoose");
-const digimon = require('./mock/digimon.json');
-const notFound = require('./middlewares/notFound')
-const error = require('./middlewares/error')
+const digimon = require("./mock/digimon.json");
+const notFound = require("./middlewares/notFound");
+const error = require("./middlewares/error");
 let CardModel = require("./model/model");
 
-const app = express();
-app.use(express.json());
+const router = express.Router();
 
-app.get("/api/cards", async (request, response, next) => {
+router.get("/api/cards", async (request, response, next) => {
   const { limit } = request.query;
   try {
     const cards = await CardModel.find({}).limit(limit || 50);
@@ -18,8 +17,8 @@ app.get("/api/cards", async (request, response, next) => {
   }
 });
 
-app.get("/api/card/:cardnumber", async (request, response, next) => {
-  try {  
+router.get("/api/card/:cardnumber", async (request, response, next) => {
+  try {
     const { cardnumber } = request.params;
     const card = await CardModel.findOne({ cardnumber: cardnumber });
     if (card) response.json(card);
@@ -29,7 +28,7 @@ app.get("/api/card/:cardnumber", async (request, response, next) => {
   }
 });
 
-app.post("/api/card", async (request, response, next) => {
+router.post("/api/card", async (request, response, next) => {
   const card = new CardModel(request.body);
   try {
     await card.save();
@@ -39,7 +38,7 @@ app.post("/api/card", async (request, response, next) => {
   }
 });
 
-app.put("/api/card/:id", async (request, response, next) => {
+router.put("/api/card/:id", async (request, response, next) => {
   const { id } = request.params;
   const card = request.body;
   const newCardInfo = {
@@ -57,46 +56,44 @@ app.put("/api/card/:id", async (request, response, next) => {
   }
 });
 
-app.delete("/api/card/:id", async (request, response, next) => {
+router.delete("/api/card/:id", async (request, response, next) => {
   const { id } = request.params;
   try {
     await CardModel.findOneAndDelete(id);
-    response
-      .status(204)
-      .send("Todo Ok");
+    response.status(204).send("Todo Ok");
   } catch (error) {
     next();
   }
 });
 
-app.post("/api/add_all_cards", async (_request, response, next) => {
+router.post("/api/add_all_cards", async (_request, response, next) => {
   const cards = digimon;
 
   try {
-    cards.map(async card => {
+    cards.map(async (card) => {
       card.quantity = 0;
       const newCard = new CardModel(card);
       await newCard.save();
-    })
-    
+    });
+
     response.send(cards);
   } catch (error) {
     next(error);
   }
 });
 
-app.delete("/api/delete_all", async (_request, response, next) => {
-  console.log('entra en delete all');
-  
+router.delete("/api/delete_all", async (_request, response, next) => {
+  console.log("entra en delete all");
+
   try {
     CardModel.deleteMany();
-    response.status(204).end()
+    response.status(204).end();
   } catch (error) {
     next(error);
   }
 });
 
-app.use(notFound);
-app.use(error);
+router.use(notFound);
+router.use(error);
 
-module.exports = app;
+module.exports = router;
